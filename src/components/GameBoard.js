665512ab34e354bd94d9214bb98f602e3d87dcd0
skarolk <skarolkiewicz@gmail.com>
2018-11-08 18:16:49 -0500

    game board component finished

--- src/components/GameBoard.js
+++ src/components/GameBoard.js
@@ -3,10 +3,29 @@ import HexTile from './HexTile';
 
 class GameBoard extends Component {
 
+  constructor() {
+    super()
+
+    this.state = {
+      tileCount: 57,
+      id: 0,
+      groundTiles: ['https://i.imgur.com/FmZU5Wp.png'],
+    }
+  }
+
+  renderBoard = () => {
+    const hexes = []
+    for (let i = 1; i < this.state.tileCount + 1; i++) {
+      hexes.push(<HexTile key={i} id={i} image={this.state.groundTiles[0]} />)
+    }
+    return hexes
+  }
 
   render() {
     return (
-      <HexTile />
+      <ul id="hexGrid">
+        {this.renderBoard()}
+      </ul>
     )
   }
 }
