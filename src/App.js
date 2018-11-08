f3f120e7bbb4b7ddf6ed8a302a72d32a06308a31
skarolk <skarolkiewicz@gmail.com>
2018-11-08 15:56:28 -0500

    tiles working

--- src/App.js
+++ src/App.js
@@ -1,12 +1,14 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
+import GameBoard from './components/GameBoard';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <header className="App-header">
+          <GameBoard />
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Edit <code>src/App.js</code> and save to reload.
---
+++ src/components/GameBoard.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import HexTile from './HexTile';
+
+class GameBoard extends Component {
+
+
+  render() {
+    return (
+      <HexTile />
+    )
+  }
+}
+
+export default GameBoard;
