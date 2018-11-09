ae4a32020007ce4254d9edd7d5463ae1276c2c5d
skarolk <skarolkiewicz@gmail.com>
2018-11-09 13:50:04 -0500

    dice added to game area

--- src/App.js
+++ src/App.js
@@ -2,13 +2,17 @@ import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import GameBoard from './components/GameBoard';
+import DiceContainer from './components/DiceContainer';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <div className="gameContainer">
-          <GameBoard />
+        <div className="playArea">
+          <DiceContainer />
+          <div className="gameContainer">
+            <GameBoard />
+          </div>
         </div>
       </div>
     );
