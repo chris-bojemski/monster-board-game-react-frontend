5ff159a7a1815992e830d983e3c6a997ea704e33
skarolk <skarolkiewicz@gmail.com>
2019-05-29 17:48:43 -0400

    minor refactoring and debugging

--- src/components/PlayerDiceContainer.js
+++ src/components/PlayerDiceContainer.js
@@ -1,13 +1,17 @@
-import React from 'react';
-import ReactDice from 'react-dice-complete';
+import React from "react";
+import ReactDice from "react-dice-complete";
 
 const PlayerDiceContainer = props => {
   const rollDoneCallback = num => {
-    props.diceValueMultiplexer(props.playerId, num)
-  }
+    props.diceValueMultiplexer(props.playerId, num);
+  };
 
   return (
-    <div className={props.playerId === 1 ? "playerOneDiceTray" : "playerTwoDiceTray"}>
+    <div
+      className={
+        props.playerId === 1 ? "playerOneDiceTray" : "playerTwoDiceTray"
+      }
+    >
       <h2 className="diceHeader">Player {props.playerId === 1 ? "1" : "2"}</h2>
       <br />
       <ReactDice
@@ -16,14 +20,26 @@ const PlayerDiceContainer = props => {
         rollDone={rollDoneCallback}
         faceColor="rgb(153,0,0)"
         dotColor="rgb(251,251,251)"
-        outline="true"
+        outline={true}
         outlineColor="rgb(102,0,0)"
         dieSize={75}
       />
-      <h3>Move:<br />{props.currentTurn.move ? props.currentTurn.move : '-'}<br /><br /></h3>
-      <h3>Attack:<br />{props.currentTurn.attack ? props.currentTurn.attack : '-'}<br /><br /></h3>
+      <h3>
+        Move:
+        <br />
+        {props.currentTurn.move ? props.currentTurn.move : "-"}
+        <br />
+        <br />
+      </h3>
+      <h3>
+        Attack:
+        <br />
+        {props.currentTurn.attack ? props.currentTurn.attack : "-"}
+        <br />
+        <br />
+      </h3>
     </div>
-  )
-}
+  );
+};
 
-export default PlayerDiceContainer
+export default PlayerDiceContainer;
